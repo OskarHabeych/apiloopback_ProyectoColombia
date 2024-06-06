085656c7b18925d6ae88fc38c210167fae698cf9
Isidro J. Gallardo Navarro <74465584+Kalixt044@users.noreply.github.com>
2024-06-06 00:49:04 -0500

    segundo commit]

--- FrontColombia/src/app.tsx
+++ FrontColombia/src/app.tsx
@@ -4,7 +4,7 @@ import './css/tailwind.css';
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world Oscar Proyecto Colombia Ready, lorem ipsum dolorus!!!
+      Hello world Oscar Proyecto Colombia Ready, lorem ipsum dolorus ipsu!
     </h1>
   );
 }
