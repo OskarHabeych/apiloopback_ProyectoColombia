5c2b75c18cd7f77243446aa8125848698f99ed95
OskarHabeych <91106352+OskarHabeych@users.noreply.github.com>
2024-06-11 11:48:51 -0500

    commit 3 : organizac componentes

---
+++ FrontColombia/src/components/footer.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div
+          id="5d3b8479-1162-4084-8ca9-1b6d6cf38b7b"
+          className="widget widget-footer widget-footer-footer-3"
+        >
+          <div
+            data-ux="Widget"
+            role="contentinfo"
+            id="5d3b8479-1162-4084-8ca9-1b6d6cf38b7b"
+            className="x-el x-el-div x-el c1-1 c1-2 c1-v c1-b c1-c c1-d c1-e c1-f c1-g c1-1 c1-2 c1-b c1-c c1-d c1-e c1-f c1-g"
+          >
+            <div>
+              {" "}
+              <section
+                data-ux="Section"
+                className="x-el x-el-section c1-1 c1-2 c1-v c1-da c1-7k c1-b c1-c c1-l c1-m c1-d c1-e c1-f c1-g"
+              >
+                <div
+                  data-ux="SectionContainer"
+                  className="x-el x-el-div c1-1 c1-2 c1-p c1-q c1-8m c1-8n c1-t c1-db c1-b c1-c c1-7l c1-d c1-7m c1-e c1-7n c1-f c1-7o c1-g"
+                >
+                  <div
+                    data-ux="Layout"
+                    className="x-el x-el-div c1-1 c1-2 c1-b c1-c c1-d c1-e c1-f c1-g"
+                  >
+                    <div
+                      data-ux="Grid"
+                      className="x-el x-el-div c1-1 c1-2 c1-14 c1-6h c1-6i c1-a8 c1-2a c1-29 c1-y c1-28 c1-16 c1-76 c1-b c1-c c1-d c1-e c1-f c1-g"
+                    >
+                      <div
+                        data-ux="GridCell"
+                        className="x-el x-el-div c1-1 c1-2 c1-6h c1-dc c1-73 c1-bh c1-t c1-37 c1-38 c1-39 c1-3a c1-b c1-c c1-d c1-dd c1-de c1-e c1-f c1-g"
+                      >
+                        <div
+                          data-ux="FooterDetails"
+                          data-aid="FOOTER_COPYRIGHT_RENDERED"
+                          data-typography="DetailsGamma"
+                          className="x-el c1-3j c1-26 c1-1t c1-1u c1-4u c1-2a c1-y c1-b c1-2b c1-3k c1-2d c1-3w c1-df c1-3x c1-3y c1-3z x-rt"
+                        >
+                          <p style="margin:0">
+                            <span>
+                              Copyright © 2024 Ripiadera - All Rights Reserved.
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                      <div
+                        data-ux="GridCell"
+                        className="x-el x-el-div c1-1 c1-2 c1-6h c1-dc c1-73 c1-bh c1-t c1-37 c1-38 c1-39 c1-3a c1-b c1-c c1-d c1-dd c1-de c1-e c1-f c1-g"
+                      >
+                        <p
+                          data-ux="FooterDetails"
+                          data-aid="FOOTER_POWERED_BY_RENDERED"
+                          data-typography="DetailsGamma"
+                          className="x-el x-el-p c1-3j c1-26 c1-1t c1-1u c1-4u c1-dg c1-y c1-b c1-2b c1-3k c1-2d c1-3w c1-dh c1-8d c1-3x c1-3y c1-3z"
+                        >
+                          <span>
+                            Powered by{" "}
+                            <a
+                              rel="nofollow noopener"
+                              role="link"
+                              aria-haspopup="true"
+                              data-ux="Link"
+                              target="_blank"
+                              data-aid="FOOTER_POWERED_BY_RENDERED_LINK"
+                              href="https://www.godaddy.com/websites/website-builder?isc=pwugc&amp;utm_source=wsb&amp;utm_medium=applications&amp;utm_campaign=en-us_corp_applications_base"
+                              data-typography="LinkAlpha"
+                              className="x-el x-el-a c1-1q c1-1r c1-1s c1-1t c1-1u c1-1v c1-1w c1-dg c1-b c1-21 c1-3k c1-22 c1-23 c1-24 c1-3w c1-dh c1-8d c1-3x c1-3y c1-3z"
+                              data-tccl="ux2.FOOTER.footer3.Layout.Default.Link.Default.443469.click,click"
+                            >
+                              GoDaddy
+                            </a>
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  data-ux="SectionContainer"
+                  className="x-el x-el-div c1-1 c1-2 c1-p c1-q c1-8m c1-8n c1-t c1-db c1-76 c1-dg c1-b c1-c c1-7l c1-d c1-7m c1-e c1-7n c1-f c1-7o c1-g"
+                >
+                  <ul
+                    data-ux="NavFooter"
+                    className="x-el x-el-ul c1-1 c1-2 c1-di c1-37 c1-38 c1-39 c1-3a c1-2a c1-29 c1-y c1-28 c1-92 c1-b c1-c c1-1j c1-ab c1-d c1-e c1-f c1-g"
+                  >
+                    <li style="display:inline-block">
+                      <a
+                        rel=""
+                        role="link"
+                        aria-haspopup="false"
+                        data-ux="NavFooterLink"
+                        data-page="6d088f36-ad73-435e-9189-a0af1c085378"
+                        target=""
+                        data-aid="FOOTER_PAGE_LINK_0_RENDERED"
+                        data-edit-interactive="true"
+                        href="/privacy-policy"
+                        data-typography="NavAlpha"
+                        className="x-el x-el-a c1-3j c1-26 c1-1s c1-1t c1-1u c1-1f c1-1w c1-5r c1-5t c1-5u c1-5s c1-b c1-2b c1-3k c1-2d c1-23 c1-3v c1-dj c1-dk c1-3w c1-3x c1-3y c1-3z"
+                        data-tccl="ux2.FOOTER.footer3.Nav.Footer.Link.Default.443470.click,click"
+                      >
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li style="display:inline-block">
+                      <a
+                        rel=""
+                        role="link"
+                        aria-haspopup="false"
+                        data-ux="NavFooterLink"
+                        data-page="7547b4f1-ee59-409c-8aa6-184f870b8660"
+                        target=""
+                        data-aid="FOOTER_PAGE_LINK_1_RENDERED"
+                        data-edit-interactive="true"
+                        href="/terms-and-conditions"
+                        data-typography="NavAlpha"
+                        className="x-el x-el-a c1-3j c1-26 c1-1s c1-1t c1-1u c1-1f c1-1w c1-5r c1-5t c1-5u c1-5s c1-b c1-2b c1-3k c1-2d c1-23 c1-3v c1-dj c1-dk c1-3w c1-3x c1-3y c1-3z"
+                        data-tccl="ux2.FOOTER.footer3.Nav.Footer.Link.Default.443471.click,click"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </section>{" "}
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
