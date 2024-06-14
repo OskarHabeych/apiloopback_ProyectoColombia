2a6683648fb55d513ee16c037e3a039c292b4210
OskarHabeych <91106352+OskarHabeych@users.noreply.github.com>
2024-06-14 11:22:52 -0500

    Creacion de los componentes del Front en React

--- proyecto_colombia/src/datasources/mongo.datasource.ts
+++ proyecto_colombia/src/datasources/mongo.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'Mongo',
   connector: 'mongodb',
-  url: 'mongodb+srv://oivansandino2:3n2GOcJ1z5XwDky0@peakus.tzvacdg.mongodb.net/',
+  url: 'mongodb+srv://oivansandino3:GA5SAtOtgQ1E5Fqh@peakus.tzvacdg.mongodb.net/',
   host: '',
   port: 0,
   user: '',
