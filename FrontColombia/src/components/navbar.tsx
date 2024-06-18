5b5da5f744aeee6887b4892303139730a455a0bd
Isidro J. Gallardo Navarro <74465584+Kalixt044@users.noreply.github.com>
2024-06-18 00:15:37 +0000

    Test gitDev

--- FrontColombia/src/components/navbar.tsx
+++ FrontColombia/src/components/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => (
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li><a>Homepage</a></li>
             <li><a>Portfolio</a></li>
-            <li><a>About</a></li>
+            <li><a>Sobre nosotros</a></li>
           </ul>
         </div>
       </div>
