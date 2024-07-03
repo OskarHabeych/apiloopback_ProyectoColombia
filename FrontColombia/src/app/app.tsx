34ad6d6ee05a867ad5082944b17503742ff442bf
Isidro J. Gallardo Navarro <74465584+Kalixt044@users.noreply.github.com>
2024-07-03 11:52:44 -0500

    Layout

--- FrontColombia/src/app/app.tsx
+++ FrontColombia/src/app/app.tsx
@@ -6,12 +6,15 @@ import SignUp from '../components/signup/index';
 import Login from '../general_pages/sign/index.tsx';
 import Carousel from '../components/carousel/index.tsx';
 import Hero from '../components/hero/index.tsx'
+import Footer from '../components/footer/index.tsx'
 import '../css/tailwind.css';
 
 function App() {
   return (
    <> 
+<Navbar/>
 <Login/>
+<Footer/>
 </>
 );
 }
---
+++ FrontColombia/src/general_pages/layout/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'; // Importa useState correctamente
+import navbar from '../../components/Navbar/index.tsx';
+import Footer from '../../components/footer/index.tsx'
+
+const Layout = () => {
+  return (
+    <main/>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </main>
+   )
+  }
+
+export default Layou
